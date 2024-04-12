37a4657c43bdf5b5855fac727b3d9140cf79a406
Roman Kondrashev <distanceOn@yandex.ru>
2024-04-12 03:03:49 +0300

    fix: delete trash

--- src/components/molecules/Search/Search.tsx
+++ src/components/molecules/Search/Search.tsx
@@ -1,12 +1,11 @@
 import { Input } from 'antd';
-import { useAppDispatch, useAppSelector } from '../../../app/reduxHooks';
+import { useAppDispatch } from '../../../app/reduxHooks';
 import { setSearch } from '../../../app/reducers/moviesSlice';
 import { debounce } from 'lodash';
-import { SetStateAction, useEffect, useRef, useState } from 'react';
+import { SetStateAction, useRef, useState } from 'react';
 
 export const Search = () => {
   const [localSearch, setLocalSearch] = useState('');
-  const { search } = useAppSelector(state => state.movies);
   const dispatch = useAppDispatch();
 
   const debouncedSetSearchRef = useRef(
@@ -14,10 +13,6 @@ export const Search = () => {
       dispatch(setSearch(value));
     }, 500)
   );
-
-  useEffect(() => {
-    console.log(search);
-  }, [search]);
 
   const handleOnChange = (event: {
     target: { value: SetStateAction<string> };
